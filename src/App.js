e70b2d18bb4b28a1b4b977b70776f960026fef83
Dev One <dev1@MacBook.local>
2020-01-09 16:04:47 -0700

    Initial upload of template react app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+           and save to reload.  Testing!
         </p>
         <a
           className="App-link"
@@ -22,5 +24,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
