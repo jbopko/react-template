f75578eb0f3202e377a7f06298350e10c1134865
Dev One <dev1@MacBook.local>
2020-01-09 17:18:41 -0700

    Cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,24 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: 'babel-loader',
       },
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(svg|png)$/,
-        use: {
-          loader: 'file-loader',
-        },
-      },
+      { test: /\.(css|scss)$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(svg|png)$/, use: 'file-loader' },
+      { test: /\.(txt|log)$/, use: 'raw-loader' },
     ],
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
+    new HtmlWebPackPlugin({ template: './src/index.html' }),
   ],
 };
